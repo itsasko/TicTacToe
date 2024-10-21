fa25a873a3f588c71682b408d9329108b0e83abc
Oksana <oksa.osidach@gmail.com>
2024-10-21 19:12:15 +0200

    filling cells with X on click

--- code/Game.cpp
+++ code/Game.cpp
@@ -7,6 +7,10 @@ Game::Game() : window(sf::VideoMode(300, 300), "fxx") {
     window.setIcon(80, 80, windowIcon.getPixelsPtr());
     background.loadFromFile("images/grid.png");
     backgroundSprite.setTexture(background);
+    x_o_s.resize(3, std::vector<sf::Sprite>(3));
+    cell_occupation_state.resize(3, std::vector<int>(3, 0));
+    x_o_texture.loadFromFile("images/x_o.png");
+    fillWithX_O();
 }
 
 void Game::run() {
@@ -29,7 +33,11 @@ void Game::processEvents() {
         case sf::Event::MouseButtonPressed:
             if (event.mouseButton.button == sf::Mouse::Left) {
                 std::pair<int, int> pair = getClickedCell();
-                std::cout << pair.first << " " << pair.second << std::endl;
+                if (!cell_occupation_state[pair.first][pair.second]) {
+                    x_o_s[pair.first][pair.second].setTextureRect(sf::IntRect(0, 81, 81, 81));
+                    cell_occupation_state[pair.first][pair.second] = 1;
+                    std::cout << pair.first << " " << pair.second << std::endl;
+                }
             }
             break;
         }
@@ -43,6 +51,11 @@ void Game::update() {
 void Game::render() {
     window.clear();
     window.draw(backgroundSprite);
+    for (auto i : x_o_s) {
+        for (auto j : i) {
+            window.draw(j);
+        }
+    }
     window.display();
 }
 
@@ -65,4 +78,14 @@ std::pair<int, int> Game::getClickedCell() {
     int col = adjustedX / (cellSize + padding);
 
     return { row, col };
+}
+
+void Game::fillWithX_O() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            x_o_s[i][j].setTexture(x_o_texture);
+            x_o_s[i][j].setPosition(27 + j * (81 + 3), 27 + i * (81 + 3));
+            x_o_s[i][j].setTextureRect(sf::IntRect(0, 0, 81, 81));
+        }
+    }
 }
--- code/Game.h
+++ code/Game.h
@@ -3,6 +3,7 @@
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 class Game
 {
@@ -14,8 +15,12 @@ private:
 	sf::Texture background;
 	sf::Sprite backgroundSprite;
 	sf::Image windowIcon;
+	std::vector<std::vector<sf::Sprite>> x_o_s;
+	std::vector<std::vector<int>> cell_occupation_state;
+	sf::Texture x_o_texture;
 
 	std::pair<int, int> getClickedCell();
+	void fillWithX_O();
 
 	void processEvents();
 	void update();
